770a8cf991087c37fe577df51d016e8302918e84
SilvaJuarezAlejandro11 <asilva1900@alumno.ipn.mx>
2022-04-26 21:11:10 -0500

    100% completado

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,9 +12,9 @@ const nodemailer = require('nodemailer');
 
 const mail = (email, id) => {
   var transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    service: 'Gmail',
+    port: 465,
+    secure: true,
     auth: {
       user: 'batiz.lab.spankybot@gmail.com',
       pass: 'Elgato66',
